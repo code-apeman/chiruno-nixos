317fb461fa9fc888233bdc8d1aed06c01dac291f
GhostNoise <dev@ghostnoise.ru>
2026-05-14 17:42:59 +0300

    Added LAN to firewall's trusted interfaces

--- config/networking/interfaces.nix
+++ config/networking/interfaces.nix
@@ -1,15 +1,18 @@
 { config, lib, pkgs, ... }: {
-  networking.useDHCP = false;
-  networking.bridges.lan.interfaces = [
-    "swnic"
-    "pcitopnic"
-    "pcibotnic"
-  ];
-  networking.interfaces = {
-    lan.ipv4.addresses = [{
-      address = "192.168.1.1";
-      prefixLength = 24;
-    }];
-    wannic.useDHCP = true;
+  networking = {
+    useDHCP = false;
+    bridges.lan.interfaces = [
+      "swnic"
+      "pcitopnic"
+      "pcibotnic"
+    ];
+    interfaces = {
+      lan.ipv4.addresses = [{
+        address = "192.168.1.1";
+        prefixLength = 24;
+      }];
+      wannic.useDHCP = true;
+    };
+    firewall.trustedInterfaces = [ "lan" ];
   };
 }
