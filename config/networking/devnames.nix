ea1bfc50d9caef1cb341039e2780fb1dd38088fa
GhostNoise <dev@ghostnoise.ru>
2026-05-31 08:02:43 +0300

    fixes

--- config/networking/devnames.nix
+++ config/networking/devnames.nix
@@ -3,7 +3,7 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:03:00.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="swnic"
     ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:04:00.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="wannic"
-    ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:06:06.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="pcitopnic"
-    ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:06:07.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="pcibotnic"
+    ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:06:06.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="pcibotnic"
+    ACTION=="add", SUBSYSTEM=="net", KERNELS=="0000:06:07.0", ATTR{mtu}="1500", ATTR{tx_queue_len}="2000", NAME="pcitopnic"
   '';
 }
--- config/services/ollama.nix
+++ config/services/ollama.nix
@@ -1,12 +1,8 @@
 { config, lib, pkgs, inputs, ... }: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      ollama = prev.ollama.override {
-        cudaArches = [ "61" ]; # 6.1 - NVIDIA Compute Capability for consumer Pascal GPUs, GTX 1070 being one of them
-      };
-    })
-  ];
-  
+  nixpkgs.config = {
+    cudaSupport = true;
+    cudaArchList = [ "6.1" ]; 
+  };  
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
